b6b96135386c7ad46be8d3881c87d96892d042e8
Sihle Sawula <150780503+sihlesawula@users.noreply.github.com>
2026-01-30 12:49:43 +0200

    updated

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
 const dropdown = document.querySelector(".dropdown");
 
-// Toggle menu
+/* Toggle menu */
+
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-// Mobile dropdown
+/* Mobile dropdown */
+
 dropdown.addEventListener("click", () => {
+
   if (window.innerWidth <= 900) {
     dropdown.classList.toggle("active");
   }
+
 });
-
-
