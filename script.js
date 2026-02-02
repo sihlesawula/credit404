11418b801986a66c65e3746bfd8445e1c2d841bd
Sihle Sawula <150780503+sihlesawula@users.noreply.github.com>
2026-02-02 14:56:50 +0200

    more additions

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ const navLinks = document.getElementById("nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+const track = document.querySelector('.testimonial-track');
+  const nextBtn = document.querySelector('.next');
+  const prevBtn = document.querySelector('.prev');
+
+  let position = 0;
+
+  const cardWidth = 345; // Card + gap
+
+  nextBtn.addEventListener('click', () => {
+    const maxScroll = track.scrollWidth - track.parentElement.offsetWidth;
+
+    position += cardWidth;
+
+    if (position > maxScroll) {
+      position = maxScroll;
+    }
+
+    track.style.transform = `translateX(-${position}px)`;
+  });
+
+  prevBtn.addEventListener('click', () => {
+
+    position -= cardWidth;
+
+    if (position < 0) {
+      position = 0;
+    }
+
+    track.style.transform = `translateX(-${position}px)`;
+  });
